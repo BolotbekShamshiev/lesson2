94707bc437a80e9227545c7802ab296aa2f81806
BolotbekShamshiev <bolotster@gmail.com>
2024-03-22 18:46:00 +0600

    first commit

---
+++ lesson2_practice.dart
@@ -0,0 +1,59 @@
+void main() {
+  int a1 = 5;
+  int a2 = 15;
+  int a3 = 25;
+  int a4 = 35;
+  int a5 = 45;
+
+  double b1 = 1.5;
+  double b2 = 1.6;
+  double b3 = 3.14;
+  double b4 = 8.001;
+  double b5 = 100.356;
+
+  num c1 = 0;
+  num c2 = 2.2;
+  num c3 = 33;
+  num c4 = 44.55;
+  num c5 = 607;
+
+  String prez1 = 'askar';
+  String prez2 = 'kurmanbek';
+  String prez3 = 'almaz';
+  String prez4 = 'sooronbai';
+  String prez5 = 'sadyr';
+
+  bool isGoodPrez = false;
+  bool isBadPrez = true;
+
+  List<String> regionsList = [
+    'Chui',
+    'Talas',
+    'Naryn',
+    'Issyk-Kul',
+    'Osh',
+    'Djalal-Abad',
+    'Batken'
+  ];
+  List<int> rateList = [1, 2, 3, 4, 5];
+  List sifraList = ['one', 2, 'tri', 4, 'five', 6, 'sem'];
+
+  Map user = {
+    'name': 'bolot',
+    'age': 37,
+    'isMan': true,
+  };
+
+  dynamic dollar = 89.5;
+  print(dollar);
+
+  var euro = 100;
+  euro = 101;
+
+  final double rubl;
+  rubl = 0.975;
+
+  const tenge = 0.22;
+
+  print(a1 + a2);
+}
